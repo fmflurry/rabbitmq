763d78919b11c39f2327e59300785209de24ecaf
fmflurry <fm.florian.michel@gmail.com>
2021-10-07 02:46:41 +0200

    Tuto-three is about pattern Publisher/Subscriber with a Fanout exchange type.

---
+++ tuto-three/Emit/EmitLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+
+namespace Emit
+{
+    class EmitLog
+    {
+		public static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using(var connection = factory.CreateConnection())
+			{
+				using(var channel = connection.CreateModel())
+				{
+					// Publishing to a non-existing exchange is forbidden, make sure to declare it
+					channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+					var message = GetMessage(args);
+					var body = Encoding.UTF8.GetBytes(message);
+					channel.BasicPublish(exchange: "logs",
+							routingKey: "",
+							basicProperties: null,
+							body: body);
+					Console.WriteLine(" [x] Sent {0}", message);
+				}
+				Console.WriteLine(" Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+
+		private static string GetMessage(string[] args)
+		{
+			return args.Any()
+					? string.Join(" ", args)
+					: "info: Hello World!";
+		}
+    }
+}
